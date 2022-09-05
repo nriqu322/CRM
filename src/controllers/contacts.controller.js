84d9ff39f64a9f431012881bb8fdf61236323a13
Luis Saavedra <nriqu322@gmail.com>
2022-09-05 01:37:19 -0600

    update contacts controller

--- src/controllers/contacts.controller.js
+++ src/controllers/contacts.controller.js
@@ -1,21 +1,96 @@
-const getAllContacts = async (req, res) => {
-  res.send('get a contact list');
+const Contacts = require('../models/Contacts');
+
+const getAllContacts = async (req, res, next) => {
+  try {
+    const allContacts = await Contacts.findAll();
+    res.json(allContacts);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getContact = async (req, res) => {
-  res.send('get a contact by id');
+const getContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await Contacts.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (contact === null) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+    return res.json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const createContact = async (req, res) => {
-  res.send('create a contact');
+const createContact = async (req, res, next) => {
+  try {
+    const {
+      fullname,
+      email,
+      phone,
+      job,
+      customerId,
+    } = req.body;
+    const newContact = await Contacts.create({
+      fullname,
+      email,
+      phone,
+      job,
+      customerId,
+    });
+    res.send(newContact);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updateContact = async (req, res) => {
-  res.send('update a contact');
+const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await Contacts.findOne({
+      where: {
+        id,
+      },
+    });
+    contact.set(req.body);
+    await contact.save();
+
+    if (contact === null) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteContact = async (req, res) => {
-  res.send('delete a contact');
+const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await Contacts.destroy({
+      where: {
+        id,
+      },
+    });
+
+    if (contact === null) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+    return res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
