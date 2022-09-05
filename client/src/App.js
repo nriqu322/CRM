14d97c105d75da759f6b8c12642bdc5e02ae1c6e
Luis Saavedra <nriqu322@gmail.com>
2022-09-05 03:03:37 -0600

    navbar navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CustomerForm from './components/CustomerForm'
 import CustomerList from './components/CustomerList'
+import ProjectList from './components/ProjectList'
+import ProjectForm from './components/ProjectForm'
+import ContactList from './components/ContactList'
+import ContactForm from './components/ContactForm'
+import MeetingList from './components/MeetingList'
+import MeetingForm from './components/MeetingForm'
 import Navbar from './components/Navbar'
 import { Container } from '@mui/material'
 
@@ -12,6 +18,12 @@ export default function App() {
         <Routes>
           <Route path='/' element={<CustomerList />} />
           <Route path='/customers/new' element={<CustomerForm />} />
+          <Route path='/projects' element={<ProjectList />} />
+          <Route path='/projects/new' element={<ProjectForm />} />
+          <Route path='/contacts' element={<ContactList />} />
+          <Route path='/contacts/new' element={<ContactForm />} />
+          <Route path='/meetings' element={<MeetingList />} />
+          <Route path='/meetings/new' element={<MeetingForm />} />
         </Routes>
       </Container>
     </Router>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,43 @@
-export default function Navbar() {
+import React from 'react'
+import { AppBar, Box, Container, Toolbar, Typography, Tabs, Tab } from '@mui/material'
+import { Link, useNavigate } from 'react-router-dom'
+
+function LinkTab(props) {
   return (
-    <div>Navbar</div>
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
+export default function Navbar() {
+  const navigate = useNavigate()
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static' color='transparent'>
+        <Container>
+          <Toolbar>
+            <Typography sx={{ flexGrow: 1 }}>
+              <Link to="/">CRM Userlab</Link>
+            </Typography>
+            <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
+              <LinkTab label="Customers" onClick={() => navigate("/")} />
+              <LinkTab label="Projects" onClick={() => navigate("/projects")} />
+              <LinkTab label="Contacts" onClick={() => navigate("/contacts")} />
+              <LinkTab label="Meetings" onClick={() => navigate("/meetings")} />
+            </Tabs>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
   )
 }
