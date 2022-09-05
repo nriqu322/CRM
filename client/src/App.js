a28a10bf773c0c4fe3276c2cb649421f8636a540
Luis Saavedra <nriqu322@gmail.com>
2022-09-05 02:10:57 -0600

    add initial paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CustomerForm from './components/CustomerForm'
 import CustomerList from './components/CustomerList'
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<CustomerList />}></Route>
+        <Route path='/' element={<CustomerList />} />
+        <Route path='/customers/new' element={<CustomerForm />} />
       </Routes>
     </Router>
   )
