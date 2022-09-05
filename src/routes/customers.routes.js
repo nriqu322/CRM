ab36632b0e836a20d2ebebdcb61b0c73d0500f4f
Luis Saavedra <nriqu322@gmail.com>
2022-09-05 01:38:17 -0600

    add route customer projects

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -5,18 +5,16 @@ const {
   createCustomer,
   updateCustomer,
   deleteCustomer,
+  getCustomerProjects,
 } = require('../controllers/customers.controller');
 
 const router = Router();
 
 router.get('/customers', getAllCustomers);
-
 router.get('/customers/:id', getCustomer);
-
 router.post('/customers', createCustomer);
-
 router.put('/customers/:id', updateCustomer);
-
 router.delete('/customers/:id', deleteCustomer);
+router.get('/customers/:id/projects', getCustomerProjects);
 
 module.exports = router;
