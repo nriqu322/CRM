bd78a0554e160c3ec80c2f6f8a56a6a984ab8765
Luis Saavedra <nriqu322@gmail.com>
2022-09-05 02:24:11 -0600

    add material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CustomerForm from './components/CustomerForm'
 import CustomerList from './components/CustomerList'
+import Navbar from './components/Navbar'
+import { Container } from '@mui/material'
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<CustomerList />} />
-        <Route path='/customers/new' element={<CustomerForm />} />
-      </Routes>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path='/' element={<CustomerList />} />
+          <Route path='/customers/new' element={<CustomerForm />} />
+        </Routes>
+      </Container>
     </Router>
   )
 }
-
